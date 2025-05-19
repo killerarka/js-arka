041719244bea61041a067d8dbc68af8b9d77f7ac
Arka Biswas <arkamakaut@gmail.com>
2025-05-19 21:31:22 +0000

    Dayatype-Summary  video no 9

---
+++ datatype-summary.js
@@ -0,0 +1,35 @@
+// Primitive 
+
+// 7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const IsLoggdIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+const bigNumber = 35856746984n
+
+
+
+// Reference ( Non Primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "nagraj", "doga"]
+let myObj = {
+    name : "Arka"
+    
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof anotherId);
