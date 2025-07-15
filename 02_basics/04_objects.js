86a14880bb35f35241474f8e980e172f48904887
killerarka <arkamakaut@gmail.com>
2025-07-16 02:12:20 +0530

    video 17 done 16072025

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,55 @@
+// Objects
+
+//const tinderUser = new Object();
+const tinderUser = {}
+
+ tinderUser.id = "123abc"
+tinderUser.name = "Arka";
+tinderUser.isLoggedIn = false;
+
+//console.log(tinderUser);
+
+const regularUser = {
+    email: "arka@tinder.com",
+    fullname: {
+        userfullname: {
+            firstname: "Arka",
+            lastname: "Biswas"
+        }
+    }
+}
+
+//console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 ={1:"a",2:"b",3:"c"}
+const obj2 ={4:"a",5:"b",6:"c"}
+
+//const obj3 = {...obj1, ...obj2}        --- main use case
+//const obj3 = { obj1, obj2 }
+const obj3 = Object.assign({}, obj1, obj2)
+//console.log(obj3);
+
+
+const users = [
+    {
+        id:1,
+        email: " ab@gmail.com"
+    },
+    {
+        id:1,
+        email: " ab@gmail.com"
+    },
+    {
+        id:1,
+        email: " ab@gmail.com"
+    },
+]
+users[1].email
+console.log(tinderUser);
+
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty("isLoggedIn"));
