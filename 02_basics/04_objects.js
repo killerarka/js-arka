0dbb2c17089df48bdd1899fcc73b0929d99d99fc
killerarka <arkamakaut@gmail.com>
2025-07-17 01:54:55 +0530

    video 18 done 17072025

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -45,11 +45,34 @@ const users = [
     },
 ]
 users[1].email
-console.log(tinderUser);
+//console.log(tinderUser);
 
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+//console.log(Object.keys(tinderUser));
+//console.log(Object.values(tinderUser));
+//console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty("isLoggedIn"));
+//console.log(tinderUser.hasOwnProperty("isLoggedIn"));
+
+
+//*******************************************************************  next video  ******
+
+const course = {
+    courseName: "Javascript",
+    price : "Rs. 1000",
+    courseInstructor: "Arka Biswas",
+}
+
+//course.courseInstructor
+
+const {courseInstructor:instructor} = course;
+//console.log(courseInstructor);
+console.log(instructor);
+
+
+//{
+ //   name : "Arka Biswas",
+ //   courseName: "Javascript",
+ //   price : "Rs. 1000",
+//}
+
