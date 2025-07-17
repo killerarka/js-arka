818526883c28cf30e505831d420050bd28e46167
killerarka <arkamakaut@gmail.com>
2025-07-18 01:53:23 +0530

    video 20 done 18072025

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,4 +1,38 @@
-console.log(A);
-console.log(R);
-console.log(K);
-console.log(A);
+
+
+function SayMyName() {
+    console.log("A");
+    console.log("R");
+    console.log("K");
+    console.log("A");
+
+}
+
+//SayMyName();
+
+//function addTwoNumbers(number1, number2) {
+ //   console.log(number1 + number2);
+//}
+
+function addTwoNumbers(number1, number2) {
+    //let result = number1 + number2;
+    //return result;
+return number1 + number2;
+
+}
+
+//addTwoNumbers(2 ,3);
+
+const result = addTwoNumbers(2, 3);
+
+//console.log("Result: " , result);
+
+
+function loginUserMessage(username){
+    if(username === undefined){
+        console.log("Please enter a username");
+        
+    return '${username} logged in!'
+}
+
+console.log(loginUserMessage());
