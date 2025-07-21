1632657f50dd0f4b3b225c9446eb0262abc19073
killerarka <arkamakaut@gmail.com>
2025-07-21 23:54:18 +0530

    video 22 done 21072025

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -2,11 +2,54 @@ let a = 300;
 
 if (true) {
     let a = 200;
-    console.log(a); // 200 (because block scoped)
+ //   console.log(a); // 200 (because block scoped)
 }
 
 
 
-console.log(a); // 300 (original outer scoped a)
+//console.log(a); // 300 (original outer scoped a)
 // console.log(b); // ReferenceError
 // console.log(c); // ReferenceError
+
+
+function one () {
+    const username = "arka";
+
+    function two() {
+        const website = "youtube";
+        console.log(username);
+    }
+
+   // console.log(website);
+
+
+    two ();
+
+}
+//one ();
+
+if (true) {
+    const username = "arka";
+    if(username === "arka"){
+        const website = " youtube";
+       // console.log(username + website);
+    }
+}
+
+
+// ***************** interesting part ***************
+
+function addone(num){
+    return num + 1;
+}
+addone(5);
+console.log(addone(5));
+
+
+const addTwo = function (num){
+    return num + 2;
+}
+addTwo(5);
+
+console.log(addone(5));
+console.log(addTwo(5));
