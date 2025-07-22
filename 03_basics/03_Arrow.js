607cff77d51253fa257d12425006a604fd0133a6
killerarka <arkamakaut@gmail.com>
2025-07-22 22:44:14 +0530

    video 23 done 22072025

--- 03_basics/03_Arrow.js
+++ 03_basics/03_Arrow.js
@@ -4,9 +4,46 @@ const user ={
 
     welcomeMessage : function(){
         console.log(`Welcome ${this.username} to our website!`);
+        console.log(this);
+        
     }   
 }
 
-user.welcomeMessage();
-user.username = "debika";
-user.welcomeMessage();
+//user.welcomeMessage();
+//user.username = "debika";
+//user.welcomeMessage();
+/*console.log(this);
+
+
+function chai (){
+    console.log(this);
+}
+chai();*/
+
+ //const chai = function(){
+//    let username = "arka";
+   /// console.log(this.username);
+//}
+
+//chai();
+
+ const chai = () => {
+    let username = "arka";
+    console.log(this.username);
+}
+
+//chai();
+
+//const addTwo = (number1, number2) => {
+  //  return number1 + number2;
+    
+//}
+
+//const addTwo = (number1, number2) => number1 + number2;
+const addTwo = (number1, number2) => ({username: "arka", price : 100});
+console.log(addTwo(200, 400));
+
+//const Array = [200 , 400 , 500 , 600];
+//Array.array.forEach(element => {
+    
+//});
